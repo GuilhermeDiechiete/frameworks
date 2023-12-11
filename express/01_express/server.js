b4a04cadc9a13d91f496ade9bc7ee1c508cfb767
Guilherme Diechiete <132406389+GuilhermeDiechiete@users.noreply.github.com>
2023-12-11 17:21:21 -0300

    delete frameworks

--- express/01_express/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function(req, res){
-    res.send('Bem Vindo ao NodeJS!')
-})
-app.listen(3333, ()=> console.log('Servidor Executando...'))
